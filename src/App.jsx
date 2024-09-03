962f8d00e5751b0cca7b9ff11f8c40d34f526146
Klomachenko <leekyumin0901@naver.com>
2024-09-03 21:48:34 +0900

    feat: AppLayout, outlet 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AppLayout from './layout/AppLayout';
-import HomePage from './pages/Homepage/Homepage';
+import HomePage from './pages/Homepage/HomePage';
 import MoviePage from './pages/Movies/MoviePage';
 import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // 홈페이지 /
 // 영화 전체보여주는 페이지 (서치) /movies
