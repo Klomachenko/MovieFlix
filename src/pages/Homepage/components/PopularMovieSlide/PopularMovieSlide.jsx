3e06725ff9438db0f08d12e035de3f95b132cdb8
Klomachenko <leekyumin0901@naver.com>
2024-09-04 17:40:39 +0900

    feat: PopularMovieSlide 구현

---
+++ src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies';
+import { Alert } from 'react-bootstrap';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 8,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+const PopularMovieSlide = () => {
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
+  console.log(data);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return <Alert variant='danger'>{error.message}</Alert>;
+  }
+
+  return (
+    <div>
+      <h3>PopularMovies</h3>
+      <Carousel
+        infinite={true}
+        centerMode={true}
+        dotListClass='movie-slider p-1'
+        itemClass='carousel-container'
+        responsive={responsive}
+      >
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default PopularMovieSlide;
