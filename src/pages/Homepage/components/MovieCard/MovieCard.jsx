752b6aba855a010f63e2780f10d598059a3da15d
Klomachenko <leekyumin0901@naver.com>
2024-09-05 00:17:49 +0900

    feat: moviecard data 불러오기 구현

--- src/pages/Homepage/components/MovieCard/MovieCard.jsx
+++ src/pages/Homepage/components/MovieCard/MovieCard.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import Badge from 'react-bootstrap/Badge';
 
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = ({ movie }) => {
+  console.log('moviecard', movie);
+  return (
+    <div
+      style={{
+        backgroundImage:
+          'url(' +
+          `https://media.themoviedb.org/t/p/w440_and_h660_face${movie.poster_path}` +
+          ')',
+      }}
+    >
+      <div>
+        <h1>{movie.title}</h1>
+        {movie.genre_ids.map((id) => (
+          <Badge bg='warning'>{id}</Badge>
+        ))}
+        <div>
+          <div>{movie.vote_average}</div>
+          <div>{movie.popularity}</div>
+          <div>{movie.adult ? 'over18' : 'under18'}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
