df0cbc7eea66b55b20ba1e3de1540433ece25dce
Klomachenko <leekyumin0901@naver.com>
2024-09-11 16:21:35 +0900

    feat: useMovieRecommendationQuery 구현 Description: id에 맞춰서 추천 영화를 불러옵니다. 확인을 위해 movieCard를 임의로 데이터 바인딩 하였습니다

--- src/pages/MovieDetail/MovieDetailPage.jsx
+++ src/pages/MovieDetail/MovieDetailPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useMovieDetailQuery } from '../../hooks/useMovieDetail';
-import { Alert } from 'react-bootstrap';
+import { Alert, Col, Row } from 'react-bootstrap';
 import { useMovieReviewQuery } from '../../hooks/useMovieReview';
+import { useMovieRecommendationQuery } from '../../hooks/useMovieRecommendation';
+import MovieCard from '../../common/MovieCard/MovieCard';
 
 const MovieDetailPage = () => {
   const { id } = useParams();
@@ -14,10 +16,13 @@ const MovieDetailPage = () => {
     isError,
     error,
   } = useMovieDetailQuery(id);
-  console.log(movieDetail);
+  console.log('상세정보', movieDetail);
 
   const { data: movieReviews } = useMovieReviewQuery(id);
-  console.log(movieReviews);
+  console.log('리뷰', movieReviews);
+
+  const { data: movieRecommendtaions } = useMovieRecommendationQuery(id);
+  console.log('추천영화', movieRecommendtaions);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -30,12 +35,19 @@ const MovieDetailPage = () => {
     <div>
       <h1>제목: {movieDetail.title}</h1>
       <div>
-        {movieReviews?.results.map((movieReview, index) => (
+        {/* {movieReviews?.results.map((movieReview, index) => (
           <div>
             <h2>리뷰어 : {movieReview.author}</h2>{' '}
             <h3>리뷰 내용 : {movieReview.content}</h3>
           </div>
-        ))}
+        ))} */}
+        <Row>
+          {movieRecommendtaions?.results.map((recommendationMovie, index) => (
+            <Col key={index}>
+              <MovieCard movie={recommendationMovie} />
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );
