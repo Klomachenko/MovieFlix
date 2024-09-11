332d30be2e5efd558dbe93d49d394c1c14e2b186
Klomachenko <leekyumin0901@naver.com>
2024-09-11 15:08:45 +0900

    feat: useMovieDetailQuery 구현 Description: movieDetail page에서 데이터를 받아올 수 있도록 구현함

--- src/pages/MovieDetail/MovieDetailPage.jsx
+++ src/pages/MovieDetail/MovieDetailPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useMovieDetailQuery } from '../../hooks/useMovieDetail';
 
 const MovieDetailPage = () => {
   const { id } = useParams();
   console.log('받아온 url 쿼리값', id);
+
+  const { data, isLoading, isError } = useMovieDetailQuery(id);
+  console.log(data);
+
   return <div>MovieDetailPage</div>;
 };
 
