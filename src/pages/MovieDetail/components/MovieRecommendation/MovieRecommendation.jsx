5500e269d497e377354d6af536300f239f9b0786
Klomachenko <leekyumin0901@naver.com>
2024-10-04 02:36:07 +0900

    style: MovieRecommendation style 설정

--- src/pages/MovieDetail/components/MovieRecommendation/MovieRecommendation.jsx
+++ src/pages/MovieDetail/components/MovieRecommendation/MovieRecommendation.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Col, Row, Alert } from 'react-bootstrap';
 import MovieCard from '../../../../common/MovieCard/MovieCard';
 import { useMovieRecommendationQuery } from '../../../../hooks/useMovieRecommendation';
+import './MovieRecommendation.style.css';
 
 const MovieRecommendation = ({ movieId }) => {
   const { data, isLoading, isError, error } =
@@ -15,9 +16,9 @@ const MovieRecommendation = ({ movieId }) => {
   }
 
   return (
-    <div>
-      <h1>Recommendated Movies</h1>
-      <Row>
+    <div className='box'>
+      <h2>Recommendated Movies</h2>
+      <Row className='movie-list'>
         {data?.results.map((recommendation, index) => (
           <Col key={index}>
             <MovieCard movie={recommendation} />
